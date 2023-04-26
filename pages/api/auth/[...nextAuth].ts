513f5e670396383935733ff0dc822f4e2cabacf7
Marcosgrimoldi <marcos.grimoldi@hotmail.com>
2023-04-26 00:24:29 -0300

    guardando cambios por si coco la caga

--- pages/api/auth/[...nextAuth].ts
+++ pages/api/auth/[...nextAuth].ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import prismadb from "../../../lib/prismadb";
 import { compare } from "bcrypt";
-
 export default NextAuth({
   providers: [
     Credentials({
       id: "credentials",
-      name: "credentials",
+      name: "Credentials",
       credentials: {
         email: {
           label: "Email",
@@ -19,28 +19,38 @@ export default NextAuth({
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) {
-          throw new Error("Email and Password required");
+          throw new Error("Email and password required");
         }
-
         const user = await prismadb.user.findUnique({
           where: {
             email: credentials.email,
           },
         });
-
         if (!user || !user.hashedPassword) {
-          throw new Error("email does not exist");
+          throw new Error("Email does not exist");
         }
 
         const isCorrectPassword = await compare(
           credentials.password,
           user.hashedPassword
         );
-
-        if(!isCorrectPassword ){
-            throw new Error('Incorrect Password')
+        if (!isCorrectPassword) {
+          throw new Error("Incorrect password");
         }
+
+        return user;
       },
     }),
   ],
+  pages: {
+    signIn: "/auth",
+  },
+  debug: process.env.NODE_ENV == "development",
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret:process.env.NEXTAUTH_JWT_SECRET, 
+  },
+  secret:process.env.NEXTAUTH_SEC
 });
