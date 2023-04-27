bbeb6711c904252993f2b616e731c2943b2265df
Marcosgrimoldi <marcos.grimoldi@hotmail.com>
2023-04-27 15:42:55 -0300

    cambios en el login , NavBar, e  Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
-import useCurrentUser from "../hooks/useCurrentUser";
+
+import Billboard from "@/components/Billboard";
+import Navbar from "../components/Navbar";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 
@@ -23,17 +25,11 @@ return {
 
 
 export default function Home() {
-  
-  const {data:user} = useCurrentUser();
-
-
-
   return (
     <>
-    <h1 className="text-2xl text-green-500">Netflix Clone </h1>
-    <p className="text-white">Logged in as : {user?.name}</p>
-    <button className="h-10 w-full bg-white" onClick={()=> signOut()}>Logout!</button>
-    
+   <Navbar/>
+    <Billboard/>
+  
     </>
   )
 }
